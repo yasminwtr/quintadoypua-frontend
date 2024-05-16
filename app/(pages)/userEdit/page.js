482c59dac2cbb653f1e1cd3b37abdf5e4fd6f188
Alyne Citta <alyne_fae@estudante.sesisenai.org.br>
2024-05-16 16:22:33 -0300

    Tela de edição de perfil 99% finalizada

--- app/(pages)/userEdit/page.js
+++ app/(pages)/userEdit/page.js
@@ -1,7 +1,8 @@
-import styles from "@/app/styles/sign.module.css";
+import styles from "@/app/styles/userEdit.module.css";
 import Image from 'next/image'
 import EyeOpen from '@/app/assets/images/EyeOpen.png'
 import EyeClose from '@/app/assets/images/EyeClose.png'
+import imgPhoto from '@/app/assets/images/camera.png'
 import sheets from '@/app/assets/images/sheets.png'
 import titleImage from '@/app/assets/images/title.png';
 
@@ -27,12 +28,19 @@ export default function SignUp() {
           />
         </div>
         <div className={styles.title}>
-          <span>Bem-Vindo à Pousada Quinta do Ypuã!</span>
+          <span>Editar perfil</span>
         </div>
         <div className={styles.containerBody}>
           <div className={styles.container}>
+            <div className={styles.iconImg}>
+              <Image
+                src={imgPhoto}
+                width={80}
+                alt="User photo"
+              /> 
+            </div>
             <div className={styles.span}>
-              <span>Faça seu cadastro aqui!</span>
+              <span>Alterar foto de perfil</span>
             </div>
             <div className={styles.containerInputName}>
               <div className={styles.inputName}>
@@ -47,16 +55,16 @@ export default function SignUp() {
             <div className={styles.containerInputText}>
               <div className={styles.inputs}>
                 <div className={styles.inputsEmail}>
-                  <label for="email">Email:</label>
+                  <label for="email">Atualizar email:</label>
                   <input type="text" id="email" name="email" className={styles.inputEmailTel}></input>
                 </div>
                 <div className={styles.inputsCellphone}>
-                  <label for="cellphone">Telefone:</label>
+                  <label for="cellphone">Atualizar telefone:</label>
                   <input type="text" id="cellphone" name="cellphone" className={styles.inputEmailTel}></input>
                 </div>
               </div>
               <div className={styles.containerPasswords}>
-                <label for="password" className={styles.labelPass}>Senha:</label>
+                <label for="password" className={styles.labelPass}>Nova senha:</label>
                 <div className={styles.password}>
                   <input type="password" id="password" name="password" className={styles.inputPasswords}></input>
                   <Image
@@ -78,10 +86,7 @@ export default function SignUp() {
             </div>
             <div className={styles.containerFeet}>
               <div className={styles.button}>
-                <button className={styles.buttonCadastrar}>Cadastrar</button>
-              </div>
-              <div className={styles.link}>
-                <a href="">Já possui uma conta? Faça seu login aqui!</a>
+                <button className={styles.buttonCadastrar}>Atualizar</button>
               </div>
             </div>
           </div>
