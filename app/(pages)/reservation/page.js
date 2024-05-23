0dd87ffe67d637228c6c317f9c643e64aa804c8b
Pietro Mendes <pablo.toldo3@outlook.com>
2024-05-22 22:07:08 -0300

    Tela reservation para o USER

---
+++ app/(pages)/reservation/page.js
@@ -0,0 +1,117 @@
+import styles from "@/app/styles/reservation.module.css";
+import Navbar from '@/app/components/Navbar/navbar.js'
+
+export default function Reservation() {
+    return (
+        <main className={styles.main}>
+            <Navbar />
+            <div className={styles.rooms}>
+                <div className={styles.header_rooms}>
+                    <div>
+                        <span>Todas acomodações</span>
+                    </div>
+                    <button>Filtrar Por</button>
+                </div>
+
+
+
+
+                <div className={styles.options}>
+                    <div className={styles.room}>
+                        <div></div>
+
+                        <div>
+                            <span>Quarto Sacada</span>
+                            <span>Ver acomodação</span>
+                        </div>
+                    </div>
+
+                    <div className={styles.room}>
+                        <div></div>
+
+                        <div>
+                            <span>Quarto Sacada</span>
+                            <span>Ver acomodação</span>
+                        </div>
+                    </div>
+
+                    <div className={styles.room}>
+                        <div></div>
+
+                        <div>
+                            <span>Quarto Sacada</span>
+                            <span>Ver acomodação</span>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+                <div className={styles.options}>
+                    <div className={styles.room}>
+                        <div></div>
+
+                        <div>
+                            <span>Quarto Sacada</span>
+                            <span>Ver acomodação</span>
+                        </div>
+                    </div>
+
+                    <div className={styles.room}>
+                        <div></div>
+
+                        <div>
+                            <span>Quarto Sacada</span>
+                            <span>Ver acomodação</span>
+                        </div>
+                    </div>
+
+                    <div className={styles.room}>
+                        <div></div>
+
+                        <div>
+                            <span>Quarto Sacada</span>
+                            <span>Ver acomodação</span>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+                <div className={styles.options}>
+                    <div className={styles.room}>
+                        <div></div>
+
+                        <div>
+                            <span>Quarto Sacada</span>
+                            <span>Ver acomodação</span>
+                        </div>
+                    </div>
+
+                    <div className={styles.room}>
+                        <div></div>
+
+                        <div>
+                            <span>Quarto Sacada</span>
+                            <span>Ver acomodação</span>
+                        </div>
+                    </div>
+
+                    <div className={styles.room}>
+                        <div></div>
+
+                        <div>
+                            <span>Quarto Sacada</span>
+                            <span>Ver acomodação</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+        </main>
+    );
+}
